ae9a63a09cad8e9f123c6a3db98810aedd871379
flaviare1s <flaviarster@gmail.com>
2025-02-08 10:03:52 -0300

    Commit inicial

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,18 @@
+'use client';
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #fff;
+  }
+
+  body, button, input {
+    font-family: 'Nunito', serif;
+  }
+`;
